c3afda32bc19f39e0caddafc61377b63abf01d0d
ajay <aspatil99451@gmail.com>
2023-05-11 23:51:21 +0530

    Activity-3 (Updated Code!)

--- LinearAlgebra/Activity-3/matrixOperation.py
+++ LinearAlgebra/Activity-3/matrixOperation.py
@@ -22,14 +22,50 @@ Formulas for matrix operations:
 """
 
 
+def inverseOfMatrix(matrix: np.array) -> list:
+    """
+    Inverse of Matrix
+    """
+
+    inversedMatrix = []
+
+    # Calculating the determinant of matrix
+    det_A = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    # Check if matrix is invertible
+    if det_A == 0:
+        print("Error: The matrix matrix is not invertible.")
+        ArithmeticError(f'This matrix of shape {np.shape(matrix)} is not an invertible matrix')
+    else:
+        # Calculate the inverse of matrix
+        a_inverse = [[matrix[1][1] / det_A, -matrix[0][1] / det_A], [-matrix[1][0] / det_A, matrix[0][0] / det_A]]
+        print("Inverse")
+        for row in a_inverse:
+            inversedMatrix.append(row)
+    return inversedMatrix
+
+
+def transposeOfMatrix(matrix: np.array) -> np.array:
+    row = len(matrix)
+    col = len(matrix[0])
+
+    transposed = [[0 for j in range(row)] for i in range(col)]
+
+    for x in range(row):
+        for y in range(col):
+            transposed[y][x] = transposed[x][y]
+
+    return np.array([transposed])
+
+
 def matrix_operations(matrix_1: np.array, matrix_2: np.array, operation: str):
 
     operations = {
         "add": matrix_1 + matrix_2,
         "scalar": 2 * matrix_1 + 5 * matrix_2,
         "prod": matrix_1.dot(matrix_2),
-        "transpose": matrix_1.transpose(),
-        "inverse": np.linalg.inv(matrix_1)
+        "transpose": transposeOfMatrix(matrix_1),
+        "inverse": inverseOfMatrix(matrix_1)
     }
 
     return operations[operation]
